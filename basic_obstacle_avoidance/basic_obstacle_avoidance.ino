58a22409e4f388a582aeae7b75b2382e32c88ab5
TheMartyLekekas <43269722+TheMartyLekekas@users.noreply.github.com>
2019-04-15 17:13:32 +0200

    IGNORE_THIS

--- basic_obstacle_avoidance/basic_obstacle_avoidance.ino
+++ basic_obstacle_avoidance/basic_obstacle_avoidance.ino
@@ -13,15 +13,14 @@ DifferentialControl control(leftMotor, rightMotor);
 //HC-SR04 front ultrasound sensor
 const int FRONT_TRIGGER_PIN = 6; //D6 pin
 const int FRONT_ECHO_PIN = 7; //D7 pin
-const unsigned int FRONT_MAX_DISTANCE = 500;
+const unsigned int FRONT_MAX_DISTANCE = 80;
 SR04 front_sensor(FRONT_TRIGGER_PIN, FRONT_ECHO_PIN, FRONT_MAX_DISTANCE);
 
 //HC-SR04 side ultrasound sensor
 const int SIDE_TRIGGER_PIN = 10; //D10 pin
 const int SIDE_ECHO_PIN = 11; //D11 pin
-const unsigned int SIDE_MAX_DISTANCE = 100;
+const unsigned int SIDE_MAX_DISTANCE = 80;
 SR04 side_sensor(SIDE_TRIGGER_PIN, SIDE_ECHO_PIN, SIDE_MAX_DISTANCE);
-//SimpleCar side_car(control);
 SimpleCar car(control);
 
 void setup() {
@@ -35,7 +34,6 @@ void loop() {
 void check(){
    int current_distance = front_sensor.getDistance();
    int side_distance = side_sensor.getDistance();
-//    Serial.println(side_distance);
     Serial.println(current_distance);
    if (current_distance > 0) {
     Serial.println("In If\n");
@@ -43,7 +41,6 @@ void check(){
    car.setSpeed(0);
   }
    if (current_distance > 20) {
-//    delay(1000);
    Serial.println("In Else if\n");
     car.setSpeed(30);
     car.setAngle(0);
