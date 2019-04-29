9b82c5807d7f68ab1fc7d47eb904985e6f4482c3
TheMartyLekekas <43269722+TheMartyLekekas@users.noreply.github.com>
2019-04-29 16:59:31 +0200

    Both Sensors working

--- basic_obstacle_avoidance/basic_obstacle_avoidance.ino
+++ basic_obstacle_avoidance/basic_obstacle_avoidance.ino
@@ -10,17 +10,18 @@ BrushedMotor leftMotor(leftMotorForwardPin, leftMotorBackwardPin, leftMotorSpeed
 BrushedMotor rightMotor(rightMotorForwardPin, rightMotorBackwardPin, rightMotorSpeedPin);
 DifferentialControl control(leftMotor, rightMotor);
 
+//HC-SR04 side ultrasound sensor
+const int SIDE_TRIGGER_PIN = 6; //D10 pin
+const int SIDE_ECHO_PIN = 7; //D11 pin
+const unsigned int SIDE_MAX_DISTANCE = 80;
+SR04 side_sensor(SIDE_TRIGGER_PIN, SIDE_ECHO_PIN, SIDE_MAX_DISTANCE);
+
 //HC-SR04 front ultrasound sensor
-const int FRONT_TRIGGER_PIN = 6; //D6 pin
-const int FRONT_ECHO_PIN = 7; //D7 pin
+const int FRONT_TRIGGER_PIN = 5; //D6 pin
+const int FRONT_ECHO_PIN = 4; //D7 pin
 const unsigned int FRONT_MAX_DISTANCE = 80;
 SR04 front_sensor(FRONT_TRIGGER_PIN, FRONT_ECHO_PIN, FRONT_MAX_DISTANCE);
 
-//HC-SR04 side ultrasound sensor
-const int SIDE_TRIGGER_PIN = 10; //D10 pin
-const int SIDE_ECHO_PIN = 11; //D11 pin
-const unsigned int SIDE_MAX_DISTANCE = 80;
-SR04 side_sensor(SIDE_TRIGGER_PIN, SIDE_ECHO_PIN, SIDE_MAX_DISTANCE);
 SimpleCar car(control);
 
 void setup() {
@@ -28,21 +29,21 @@ Serial.begin(9600);
 }
 
 void loop() {
- check();
+  check();
 }
 
 void check(){
    int current_distance = front_sensor.getDistance();
    int side_distance = side_sensor.getDistance();
-    Serial.println(current_distance);
-   if (current_distance > 0) {
-    Serial.println("In If\n");
-   car.setAngle(0);
-   car.setSpeed(0);
-  }
-   if (current_distance > 20) {
-   Serial.println("In Else if\n");
-    car.setSpeed(30);
+   Serial.println(current_distance);
+   Serial.println(side_distance);
+   Serial.println("KKKKKKKKKKKKKKK");
+    
+   if ((current_distance > 30 || current_distance == 0) && (side_distance > 30 || side_distance == 0)) {
+    car.setSpeed(40);
     car.setAngle(0);
   }
+   else if ((current_distance > 0 && current_distance < 30) || (side_distance > 0 && side_distance < 30)) {
+     car.setSpeed(0);
+   }
   }
