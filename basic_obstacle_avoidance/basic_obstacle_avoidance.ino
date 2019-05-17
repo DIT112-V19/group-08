ee6a63dc089d6ebf16e8b0aca2aa4568d8ec0bcc
TheMartyLekekas <43269722+TheMartyLekekas@users.noreply.github.com>
2019-05-17 18:45:30 +0200

    Rear sensor and remote control shell

--- basic_obstacle_avoidance/basic_obstacle_avoidance.ino
+++ basic_obstacle_avoidance/basic_obstacle_avoidance.ino
@@ -1,5 +1,7 @@
 #include <Smartcar.h>
 #include <SoftwareSerial.h>
+char command;
+String string;
 
 int leftMotorForwardPin = 8;
 int leftMotorBackwardPin = 10;
@@ -20,34 +22,33 @@ BrushedMotor leftMotor(leftMotorForwardPin, leftMotorBackwardPin, leftMotorSpeed
 BrushedMotor rightMotor(rightMotorForwardPin, rightMotorBackwardPin, rightMotorSpeedPin);
 DifferentialControl control(leftMotor, rightMotor);
 
-//HC-SR04 side ultrasound sensor
+//HC-SR04 side, front and back ultrasound sensors respectively
 const int SIDE_TRIGGER_PIN = 6; //D6 pin
 const int SIDE_ECHO_PIN = 7; //D7 pin
 const unsigned int SIDE_MAX_DISTANCE = 80;
 SR04 side_sensor(SIDE_TRIGGER_PIN, SIDE_ECHO_PIN, SIDE_MAX_DISTANCE);
 
-//HC-SR04 front ultrasound sensor
 const int FRONT_TRIGGER_PIN = 5; //D5 pin
 const int FRONT_ECHO_PIN = 4; //D4 pin
 const unsigned int FRONT_MAX_DISTANCE = 80;
 SR04 front_sensor(FRONT_TRIGGER_PIN, FRONT_ECHO_PIN, FRONT_MAX_DISTANCE);
+
+const int REAR_TRIGGER_PIN = 22; //D22 pin
+const int REAR_ECHO_PIN = 24; //D24 pin
+const unsigned int REAR_MAX_DISTANCE = 80;
+SR04 rear_sensor(REAR_TRIGGER_PIN, REAR_ECHO_PIN, REAR_MAX_DISTANCE);
+
 
 DirectionlessOdometer odometer(80);
 const int odometerPin = 2; //D2 pin
 
 SimpleCar car(control);
 
-
 int side_obstacle_counter = 0;
 const int blinkInterval = 600;
 boolean objectNextTo = false;
-const int rightTurnInterval = 70;
-const int leftTurnInterval = 96;
-const int backwardsInterval = 120;
-unsigned int currentdistance= 0;
 int traveled_distance = 0;
 int failproof_distance = 100;
-
 
 SoftwareSerial EEBlue(19, 20);
 
@@ -55,25 +56,63 @@ void setup() {
   pinMode(40, OUTPUT);
   digitalWrite(40,HIGH);
   Serial.begin(9600);
-
   EEBlue.begin(38400);
-  Serial.println("Connect");
 
+  Serial.println("Connect");
   pinMode(ledPin, OUTPUT);
-  odometer.attach(odometerPin, [](){
+
+  odometer.attach(odometerPin, []() {
     odometer.update();
   });
 }
 
 void loop() {
+
+  if (Serial.available() > 0)
+  {string = "";}
+
+  while(Serial.available() > 0) {
+    command = ((byte)Serial.read());
+
+    if(command == ':') {
+      break;
+    }else {
+      string += command;
+    }
+
+    if(string == "F") {
+      forward();
+      Serial.println(string);
+    }
+
+    if(string == "B"){
+      backward();
+      Serial.println(string)
+    }
+
+    if(string == "R") {
+      right();
+      Serial.println(string);
+    }
+
+    if(string == "L") {
+      left();
+      Serial.println(string);
+    }
+  }
+
   if(EEBlue.available())
   Serial1.write(EEBlue.read());
   if(Serial1.available())
   Serial2.write(Serial.read());
-  check();
+  // check();
+  // Serial.println(side_obstacle_counter);
+  // Serial.println(odometer.getDistance());
+  Serial.println(front_sensor.getDistance());
+  Serial.println(side_sensor.getDistance());
+  Serial.println(rear_sensor.getDistance());
+  Serial.println();
 
-  Serial.println(side_obstacle_counter);
-  Serial.println(odometer.getDistance());
   led();
 }
 
@@ -93,36 +132,17 @@ void led(){
     // set the LED with the ledState of the variable:
     digitalWrite(ledPin, ledState);
   }
-
 }
-
-void led(){
-   unsigned long currentMillis = millis();
-
-  if (currentMillis - previousMillis >= interval) {
-    // save the last time you blinked the LED
-    previousMillis = currentMillis;
-
-    // if the LED is off turn it on and vice-versa:
-    if (ledState == LOW) {
-      ledState = HIGH;
-    } else {
-      ledState = LOW;
-    }
-    // set the LED with the ledState of the variable:
-    digitalWrite(ledPin, ledState);
-    }
-  }
 
 void check(){
    int current_distance = front_sensor.getDistance();
    int side_distance = side_sensor.getDistance();
+   int rear_distance = rear_sensor.getDistance();
    if ((current_distance > 20 || current_distance == 0) && (side_distance > 20 || side_distance == 0)) {
     if (side_obstacle_counter == 1 && side_distance > 20){
        parallelParking();
      }
      car.setSpeed(30);
-     car.setAngle(0);
    }
 
    else if (current_distance > 0 && current_distance < 20) {
@@ -143,14 +163,34 @@ void parallelParking(){
      car.setSpeed(30);
      car.setAngle(55);
   }
+
   int  second_Distance_Stamp = odometer.getDistance();
   double  turning_Distance_Stamp = (second_Distance_Stamp - distance_Stamp) * 0.7;
-  while (odometer.getDistance() < second_Distance_Stamp + turning_Distance_Stamp){
+    while (odometer.getDistance() < second_Distance_Stamp + turning_Distance_Stamp){
      car.setSpeed(30);
      car.setAngle(-55);
   }
+
   while(1) {
     car.setSpeed(0);
     car.setAngle(0);
   }
+}
+
+void forward() {
+  car.setSpeed(30);
+}
+
+void backward() {
+  car.setSpeed(-30);
+}
+
+void right() {
+  car.setSpeed(30);
+  car.setAngle(50);
+}
+
+void left() {
+  car.setSpeed(30);
+  car.setAngle(-50);
 }
